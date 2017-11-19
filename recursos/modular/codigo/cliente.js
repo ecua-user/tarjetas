0df4260025a89cddae8674e4ae0f3e8d0713d3d6
ecua-user <juanmanuelco@yahoo.es>
2017-11-19 16:56:05 -0500

    Cliente añadido

--- recursos/modular/codigo/cliente.js
+++ recursos/modular/codigo/cliente.js
@@ -17,6 +17,54 @@ function activar_tarjeta(event){
             swal('Error', 'Asegurese de que esta tarjeta haya sido comprada para este usuario, que no haya sido previamente activada o que el código de activación este correcto', 'error')
             return;
         }
-        
+        no_cargando()
+        var cadena=''
+        for(var i=0;i < datos[0].locales.length;i++){
+            cadena+=`<div class="row" style="margin:12px; "><div class="col-lg-4 col-md-4 col-sm-3">
+                        <h3 class="centrado">${datos[1][i].nombre}</h3>
+                        <img width="100%" src="${datos[1][i].logotipo}"/><br>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-sm-9">
+                        <div class="table">
+                            <table class="table">
+                                <tr class="pinfor centrado"><th style="width:20%">Imágen</th><th style="width:40%">Beneficio</th><th style="width:40%">Restricciones</th></tr>
+                                ${obtener_beneficio(datos[0].locales[i].beneficio)}
+                            </table
+                        </div>
+                    </div></div>`
+        }
+        innerTexto('detalles_beneficios', cadena)
+        innerTexto('num_trj', datos[0].numero)
+        $('#modal_tarjeta').modal()
+    })
+}
+
+function obtener_beneficio(elementos){
+    var cadena=''
+    for(var i=0; i< elementos.length;i++){
+        cadena+=`<tr  class="centrado">
+                    <td> <img width="100%" src="${elementos[i].imagen}"/></td>
+                    <td>${elementos[i].beneficio}</td>
+                    <td>${elementos[i].restriccion}</td>
+                </tr>`
+    }
+    return  cadena
+}
+
+function finalizar_activacion(){
+    var envio={numero: obtenerTexto('num_trj')}
+    cargando()
+    $.ajax({
+		method: "POST",
+		url: "/cliente/finalizar-activacion",
+		data: envio
+	}).done(( datos )=>{
+        no_cargando()
+        if(datos=='Error')
+            swal('Error', 'Ha ocurrido un error inesperado','error')
+        else{
+            swal('Listo', datos, 'success')
+        }
+        location.reload()
     })
 }
--- rutas/cliente.js
+++ rutas/cliente.js
@@ -27,30 +27,69 @@ router.get('/',(req,res)=>{
 	res.redirect('/cliente/perfil')
 })
 
-router.get('/perfil',ensureAuthenticated,(req,res)=>{ res.render('cliente/perfil') })
+router.get('/perfil',ensureAuthenticated,(req,res)=>{ 
+	Tarjeta.find().where({cliente:req.user.username}).exec((error, tarjetas)=>{
+		console.log(tarjetas)
+		res.render('cliente/perfil') 
+	})
+})
 
 router.get('/comprado', ensureAuthenticated, (req,res)=>{
     res.render('cliente/activar')
 })
 
 router.post('/comprobar',ensureAuthenticated ,(req,res)=>{
-	console.log(req.body)
 	Tarjeta.findOne().where({$and:[
 		{numero:req.body.numero},
 		{cliente: req.user.username},
 		{activo:false},{vendida:true},
 		{activacion: req.body.activacion}
 	]}).exec((error, tarjeta)=>{
+		var respuesta= new Array()
 		if(error)
 			res.send('Error')
 		else{
 			if(tarjeta==null)
 				res.send('No se puede activar')
-			else
-				res.send(tarjeta)
+			else{
+				var locales= new Array()
+				for(var i=0; i< tarjeta.locales.length;i++){
+					locales.push({codigo: tarjeta.locales[i].local})
+				}
+				User.find().where({$and:[{eslocal:true}, {$or:locales}]}).select('nombre logotipo').exec((error, loc)=>{
+					if(error)
+						res.send('Error')
+					else{
+						respuesta.push(tarjeta)
+						respuesta.push(loc)
+						res.send(respuesta)
+					}
+				})
+			}	
 		}
 	})
 })
+
+router.post('/finalizar-activacion', ensureAuthenticated, (req,res)=>{
+	Tarjeta.findOne().where({$and:[{numero:req.body.numero},{cliente: req.user.username},{activo:false}]}).exec((error, respuesta)=>{
+		if(error)
+			res.send('Error')
+		else{
+			if(respuesta==null)
+				res.send('Error')
+			else{
+				Tarjeta.findOneAndUpdate({$and:[{numero:req.body.numero},{cliente: req.user.username},{activo:false}]},{activo:true, activacion: cadenaAleatoria()}, (error, respuesta)=>{
+					if(error)	
+						res.send('Error')
+					else
+						res.send('Activado con éxito')
+				})
+			}
+		}
+	})
+})
+
+
 
 //Permite el enrutamiento______________________________________________________________________________
 module.exports = router;
